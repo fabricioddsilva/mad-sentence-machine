06c3d14bf8f8437defb2ff0f92aa212c8ea55dfe
Fabricio Dias <fabriciodsilva3404@gmail.com>
2024-12-03 23:49:04 -0300

    .

--- src/main/java/com/kaos/madsentencemachine/config/CorsConfiguration.java
+++ src/main/java/com/kaos/madsentencemachine/config/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("${front-url}")
+                .allowedOrigins("https://mad-sentence-machine.vercel.app/")
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
